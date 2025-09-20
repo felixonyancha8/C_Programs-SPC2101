4ee7ecd145b6a92defb072f87a346b3ca540a524
felixonyancha8 <felixonyancha8@gmail.com>
2025-09-20 06:14:00 -0700

    Add files via upload

---
+++ units.c
@@ -0,0 +1,20 @@
+//program to display all the units in semester 1
+/*
+NAME: Felix Onyancha Moinde
+REG NO: PA106/G/28821/25
+DESCRIPTION: Program displaying all the units in semester 1
+*/
+
+#include <stdio.h>
+
+int main(){
+	printf(" SPM 2100: Descrete Mathematics");
+	printf("\n SPM 2104: Mathematics for Science");
+	printf("\n UCU 2105: Fundamentals of ICT");
+	printf("\n SPI 2102: Computer Systems and Organisations");
+	printf("\n UCU 2101: Communication Skills");
+	printf("\n SPC 2101: Introduction to Programming & Problem Solving");
+	printf("\n SSE 2101: Fundamentals of Systems Analysis & Design");	
+	
+	return 0;
+}
