9ea2910b1b0afb256f1de2348346125ac0ca2949
felixonyancha8 <felixonyancha8@gmail.com>
2025-09-22 15:19:25 +0300

    Add files via upload

---
+++ user_prompt.c
@@ -0,0 +1,22 @@
+/*
+NAME:Felix Onyancha Moinde 
+REG NO:PA106/G/28821/25
+DESCRIPTION:Prompting the user to enter name
+*/
+
+#include<stdio.h>
+
+int main(){
+    
+    char name[50];
+    //prompt the user to enter name 
+    printf("Enter your name: ");
+    scanf("%s", &name);
+    
+    printf("\nYour regno is PA106/G/28821/25");
+    printf("\nYour height is 5'8");
+    printf("\nYour account balance is 26500");
+    printf("\nYou are 19 yrs old");
+    
+    return 0
+}
